0394923b5bc21c1b32870acc3fed3ca5a70c746a
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2022-01-28 20:03:21 +0100

    Last section done, Error pages added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify, app,make_response
+from flask import Flask, redirect, url_for, render_template, request,app,make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def show_index():
     return render_template("about.html")
 
+@app.errorhandler(403)
+def forbidden(e):
+    app.logger.error(f"Forbidden acess:{e},route:{request.url}")
+    return render_template("403.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def server_error(e):
+    app.logger.error(f"Server error:{e},route:{request.url}")
+    return render_template("500.html")
 if __name__ == '__main__':
     app.run(debug=True)
